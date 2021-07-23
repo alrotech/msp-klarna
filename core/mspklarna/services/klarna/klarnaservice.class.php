4cb36b718bcdb97e28a15cb4f4a8dd8726c908f8
Ivan Klimchuk <ivan@klimchuk.com>
2021-07-24 00:02:20 +0300

    feat: getting klarna payment page, prototype

--- core/mspklarna/services/klarna/klarnaservice.class.php
+++ core/mspklarna/services/klarna/klarnaservice.class.php
@@ -10,6 +10,8 @@ declare(strict_types = 1);
 use alroniks\mspklarna\KlarnaGatewayInterface as Klarna;
 use Fig\Http\Message\RequestMethodInterface;
 use GuzzleHttp\Client;
+use League\Uri\Uri;
+use League\Uri\UriTemplate;
 
 require __DIR__ . '/../../vendor/autoload.php';
 
@@ -33,6 +35,38 @@ class KlarnaService
         $this->modx->lexicon->load('mspklarna:default');
     }
 
+    public function requestRedirect(string $sid)
+    {
+
+        $uri = Uri::createFromBaseUri(
+            (new UriTemplate('/payments/v1/sessions/{kp_session_id}'))->expand(['kp_session_id' => $sid]),
+            $this->config[Klarna::OPTION_GATEWAY_URL]
+        );
+
+        echo $uri;
+
+        return $this->getClient()->request(
+            RequestMethodInterface::METHOD_POST,
+            '/hpp/v1/sessions',
+            [
+                'auth' => [
+                    $this->config[Klarna::OPTION_USERNAME],
+                    $this->config[Klarna::OPTION_PASSWORD],
+                ],
+                'json' => [
+                    'payment_session_url' => (string)$uri,
+//                    'merchant_urls' => [
+//                        'success' => '',
+//                        'cancel' => '',
+//                        'back' => '',
+//                        'failure' => '',
+//                        'error' => ''
+//                    ]
+                ]
+            ]
+        );
+    }
+
     /**
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
@@ -53,9 +87,16 @@ class KlarnaService
                     'purchase_country' => 'GB',
                     'purchase_currency' => 'GBP',
                     'locale' => 'en-GB',
-                    'order_amount' => 10,
+                    'order_amount' => 1000,
                     'order_tax_amount' => 0,
-
+                    'order_lines' => [
+                        [
+                            'name' => 'Некий товар',
+                            'quantity' => 2,
+                            'total_amount' => 1000,
+                            'unit_price' => 500,
+                        ]
+                    ]
                 ]
             ]
         );
@@ -76,8 +117,11 @@ class KlarnaService
             $this->config[Klarna::OPTION_GATEWAY_URL] = $this->config[Klarna::OPTION_GATEWAY_URL_TEST];
         }
 
-        // need replace region
-
+        $this->config[Klarna::OPTION_GATEWAY_URL] = str_replace(
+            '<region>',
+            $this->config[Klarna::OPTION_GATEWAY_REGION],
+            $this->config[Klarna::OPTION_GATEWAY_URL]
+        );
     }
 
     protected function getClient(): Client

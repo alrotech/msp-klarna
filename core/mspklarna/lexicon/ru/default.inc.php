7495cc72a685cfc47b7423d20dc89e1999c0f07f
Ivan Klimchuk <ivan@klimchuk.com>
2021-08-02 23:59:49 +0300

    New translations default.inc.php (Russian)

---
+++ core/mspklarna/lexicon/ru/default.inc.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright (c) Ivan Klimchuk - All Rights Reserved
+ * Unauthorized copying, changing, distributing this file, via any medium, is strictly prohibited.
+ * Written by Ivan Klimchuk <ivan@klimchuk.com>, 2021
+ */
+
+/** @var Array $_lang */
+
+$_lang = array_merge($_lang, [
+    'klarna' => 'Klarna',
+    'klarna_about' => 'k about',
+]);
